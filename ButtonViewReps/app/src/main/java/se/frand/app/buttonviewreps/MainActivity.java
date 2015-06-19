2ac50bfed0048b4aed14ec49095cbb2f08e786ad
Victor Frandsen <mrweektor@gmail.com>
2015-06-19 00:24:01 -0700

    Two buttons with onClickListeners that increment the displayed number

---
+++ ButtonViewReps/app/src/main/java/se/frand/app/buttonviewreps/MainActivity.java
@@ -0,0 +1,48 @@
+package se.frand.app.buttonviewreps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends Activity {
+
+    private int clicks0 = 0;
+    private int clicks1 = 0;
+    private Button button0;
+    private Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        button0 = new Button(this);
+        button0.setText("" + clicks0);
+        layout.addView(button0);
+        button1 = new Button(this);
+        button1.setText("" + clicks1);
+        layout.addView(button1);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicks0++;
+                button0.setText("" + clicks0);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicks1++;
+                button1.setText("" + clicks1);
+            }
+        });
+
+        setContentView(layout);
+
+    }
+}
