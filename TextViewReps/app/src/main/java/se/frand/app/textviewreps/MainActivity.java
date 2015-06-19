d359da03de250f80b85cfdf86e9c70c541875166
Victor Frandsen <mrweektor@gmail.com>
2015-06-18 22:57:11 -0700

    Generate and show two text views

--- TextViewReps/app/src/main/java/se/frand/app/textviewreps/MainActivity.java
+++ TextViewReps/app/src/main/java/se/frand/app/textviewreps/MainActivity.java
@@ -2,6 +2,8 @@ package se.frand.app.textviewreps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -9,6 +11,26 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //create our views programmatically
+        TextView a = new TextView(this);
+        a.setText("A");
+        TextView b = new TextView(this);
+        b.setText("B");
+
+        //we do not have a resource file so we will make our layout object here
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        //add the views to our layout
+        layout.addView(a);
+        layout.addView(b);
+
+        //finally add the layout as the content view
+        //Layout extends View so setContentView(View) is used
+        setContentView(layout);
     }
+
+
+
 }
